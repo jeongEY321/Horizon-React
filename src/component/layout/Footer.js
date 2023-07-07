ab168eed71b140c5207d6802514817421163356c
changju <changju7857@gmail.com>
2023-07-07 10:42:15 +0900

    # 23/07/07 footer design modify

--- src/component/layout/Footer.js
+++ src/component/layout/Footer.js
@@ -33,33 +33,43 @@ export default function Footer({change}) {
                     <Button className='ft-btn plant-btn mercury-btn' value="mercury" onClick={change}>
                       Mercury
                     </Button>
+                    
                     <Button className='ft-btn plant-btn venus-btn' value="venus" onClick={change}>
                       Venus
                     </Button>
+                    
                     <Button className='ft-btn plant-btn earth-btn' value="earth" onClick={change}>
                       Earth
                     </Button>
+                    
                     <Button className='ft-btn plant-btn mars-btn' value="mars" onClick={change}>
                       Mars
                     </Button>
+                    
                     <Button className='ft-btn plant-btn jupiter-btn' value="jupiter" onClick={change}>
                       Jupiter
                     </Button>
+                    
                     <Button className='ft-btn plant-btn saturn-btn' value="saturn" onClick={change}>
                       Saturn
                     </Button>
+                    
                     <Button className='ft-btn plant-btn uranus-btn' value="uranus" onClick={change}>
                       Uranus
                     </Button>
+                    
                     <Button className='ft-btn plant-btn neptune-btn' value="neptune" onClick={change}>
                       Neptune
                     </Button>
+                    
                     <Button className='ft-btn plant-btn moon-btn' value="moon" onClick={change}>
                       Moon
                     </Button>
+                    
                     <Button className='ft-btn plant-btn sun-btn' value="sun" onClick={change}>
                       Sun
                     </Button>
+                    
                     <Button className='ft-btn plant-btn all-btn' value="all" onClick={change}>
                       All
                     </Button>
