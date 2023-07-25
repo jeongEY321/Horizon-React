933cceda19c06199bc15d896d5b560c59fcf3291
jeongEY <kingship89@naver.com>
2023-07-25 16:00:15 +0900

    23/07/25 before pull

--- src/App.js
+++ src/App.js
@@ -3,19 +3,18 @@ import SolarSystemModel from "./components/solarsystem/js/SolarSystemModel";
 import CSMain from "./components/constellation/js/CSMain";
 import MainScreen from "./components/main/js/MainScreen";
 import StoreMain from "./components/store/js/StoreMain";
-import { Route } from "react-router";
-import { Routes } from "react-router";
+import { Route, Routes } from "react-router";
 import Basket from "./components/store/js/Basket";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<MainScreen />} />
-        <Route path='/SolarSystem' element={<SolarSystemModel />} />
-        <Route path='/News' element={<CSMain />} />
-        <Route path='/Store' element={<StoreMain />} />
-        <Route path='/Basket' element={<Basket />} />
+        <Route path="/" element={<MainScreen />} />
+        <Route path="/SolarSystem" element={<SolarSystemModel />} />
+        <Route path="/News" element={<CSMain />} />
+        <Route path="/Store" element={<StoreMain />} />
+        <Route path="/Basket" element={<Basket />} />
       </Routes>
     </>
   );
